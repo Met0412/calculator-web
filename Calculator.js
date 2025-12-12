7344e1038ad2e940f78dd9349ea0336105917f89
Met <keithmatthew0412@gmail.com>
2025-12-12 12:18:06 +0700

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const lastChar = display.value.slice(-1);
+    if ('+-*/'.includes(lastChar)) {
+        display.value = display.value.slice(0, -1) + operator;
+    } else {
+        display.value += operator;
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
